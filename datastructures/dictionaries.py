3f7313057061be174eb8830e6b40d1a94fc5d6af
Vik-Geerawor <sgeerwor@gmail.com>
2023-03-06 23:42:05 +0000

    advanced loops, conditionals and operators

---
+++ datastructures/dictionaries.py
@@ -0,0 +1,34 @@
+def dict_demo():
+    """
+    Usage: storing a value with some key and extracting the value given the key.
+    key must be immutable    
+    """
+
+    data = {'id': 2, 'name': "Vik", 'email': "vik.geerawor@email.com"}
+    print(f"data: {data}")
+
+    data['salary'] = 75000
+    print(f"Added salary: {data}")
+
+    data['name'] = "Vik Geerawor"
+    print(f"Updated name: {data}")
+
+    keys = list(data)
+    print(f"Keys: {keys}")
+
+
+def dict_func():
+    list_of_tuples = [('APPL', 204), ('IBM', 42), ('MSFT', 32)]
+    share_prices = dict(list_of_tuples)
+    print(f"Share price: {share_prices}")
+
+
+def dict_comprehension():
+    times_2_table = {x: x * 2 for x in range(10)}
+    print(f"Times 2 table: {times_2_table}")
+
+
+def main():
+    # dict_demo()
+    # dict_func()
+    dict_comprehension()
