f5c40c1d8e7fde25f46f35981ce3a9f2cd62f5b7
Vik-Geerawor <sgeerwor@gmail.com>
2023-03-27 21:16:36 +0100

    closures and lambda added

---
+++ functions/closures.py
@@ -0,0 +1,57 @@
+import os
+import time
+
+
+def outer_func():
+    """
+    inner func needs to be called multiple update the same variable
+    critical parts:
+    1. the variable/s maintaining the state
+    2. the inner function - closure func
+    3. both need to be bundled together
+    """
+    list1 = []
+
+    def inner_func(x):
+        list1.append(x)
+        print(list1)
+
+    return inner_func
+
+
+def make_greetings(names):
+    """
+    Internal python design of variable sharing leads to unexpected results
+    When each of the functions from the list is actually called
+    the variable `name` only has 1 value
+    and that is 'Jeniffer'
+    Lesson: Never even used a non-local or global var
+    """
+    funcs = []
+
+    for name in names:
+        funcs.append(lambda: print(f"Hello {name}"))
+        # funcs.append(lambda name=name: print(f"Hello {name}"))    # NOTE: Fix
+
+    # Testing
+    # for i in range(len(funcs)):
+    #     funcs[i]()
+
+    return funcs
+
+
+def main():
+    os.system('clear')
+
+    # add_num = outer_func()
+    # add_num(1)
+    # add_num(2)
+    # add_num(3)
+
+    a, b, c = make_greetings(['Vik', 'Brian', 'Jeniffer'])
+    a()
+    b()
+    c()
+
+
+main()
--- functions/lambdas.py
+++ functions/lambdas.py
@@ -1,10 +1,45 @@
+import os
+from functools import partial
+
+
 def power_of(n):
     """
-    n - is the exponent and is fixed at the point of the function call
-    The function returns another function with the ability to calculate
-    the power of the fixed value of n, e.g. 2 (power of 2)
-    x - is the variable which is then passed to the above function to calc
-    it's power of the fixed value of n, i.e. 3 to the power of 2
-
+    returns a function which:
+    1. accepts an arg x, and
+    2. returns a value raised to the power of n
     """
     return lambda x: x ** n
+
+
+def lambda_delayed():
+    """
+    returns a func which takes 2 numbers and returns their sum
+    """
+    def add(a, b):
+        return a + b
+
+    a = 2
+    b = 3
+    def f(): return add(a, b)
+    g = partial(add, a, b)
+
+    a = 20
+    b = 30
+
+    x = f()
+    y = g()
+    print(f"lambda: x = {x}")
+    print(f"partial: y = {y}")
+
+
+def main():
+    os.system('clear')
+
+    # power_of_two = power_of(2)
+    # a = power_of_two(3)
+    # print(a)
+
+    lambda_delayed()
+
+
+main()
