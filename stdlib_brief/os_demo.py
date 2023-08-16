2e22ab07436c8018123faf5b758a89c73f20b1f3
Vik Geerawor <sgeerawor@gmail.com>
2023-08-16 21:23:03 +0100

    update os module

--- stdlib_brief/os_demo.py
+++ stdlib_brief/os_demo.py
@@ -1,19 +1,47 @@
 import os
 
 
-def os_demo():
-    """
-    functions for interacting with the operating system
-    """
-    print(f"getcwd: {os.getcwd()}")
-    os.chdir('stdlib')
-    print(f"after chdir: {os.getcwd()}")
-    # NOTE: executes the string in the sys shell
-    os.system('touch test_file.txt')
-    # NOTE: returns a list of all module funcs
-    print(f"{dir(os)}")
-    print(f"{help(os)}")
+def get_cwd():
+    """Return current working directory."""
+    pwd = os.getcwd()
+    return pwd
+
+
+def get_files(my_path):
+    """Return the content on a path."""
+    if os.path.isdir(my_path):          # NOTE: isdir needs abs path
+        files = os.listdir(my_path)
+        return files
+
+
+def get_files_abs(my_path):
+    """Return the absolute path of the content on a path."""
+    files = get_files(my_path)
+    files = [os.path.abspath(file) for file in files]
+    return files
+
+
+def get_dirs(my_path):
+    """Return the directories on a path"""
+    dirs = [file for file in get_files_abs(my_path) if os.path.isdir(file)]
+    return dirs
 
 
 if __name__ == '__main__':
-    os_demo()
+    print(f"*** Current Directory ***")
+    my_path = get_cwd()
+    print(f"current directory: {my_path}")
+
+    print(f"*** Files ***")
+    # files = get_files(my_path)
+    # for file in files:
+    #     print(f"{file}")
+
+    files = get_files_abs(my_path)
+    for file in files:
+        print(f"{file}")
+
+    print(f"*** Directories ***")
+    dirs = get_dirs(my_path)
+    for dir in dirs:
+        print(f"{dir}")
