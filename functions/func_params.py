4b14e7623456b39170f711d9820fa2553a539300
Vik Geerawor <sgeerwor@gmail.com>
2024-05-13 11:19:34 +0100

    func_params.py reviewed

--- functions/func_params.py
+++ functions/func_params.py
@@ -22,14 +22,6 @@ def keyword_only(*, a: int = None, b: int) -> None:
     print(f"{a=} {b=}")
 
 
-def combined_example(pos_only, /, pos_or_kwd, *, kwd_only):
-    """
-    pos-only before /
-    keyword-only after *
-    """
-    print(f"{pos_only=}, {pos_or_kwd=}, {kwd_only=}")
-
-
 # Variable Length Arguments
 
 def args_params(*x: int) -> None:
@@ -46,23 +38,33 @@ def kwargs_params(**x: int) -> None:
     print(f"{type(x)=} {x=}")
 
 
+def combined_example(pos_only, /, pos_or_kw, *, kw_only, **kwargs):
+    """
+    pos-only before /
+    keyword-only after * - N.B. *args cannot be used in this case
+    """
+    print(f"{pos_only=}, {pos_or_kw=}, {kw_only=}, {kwargs=}")
+
+
 if __name__ == "__main__":
     os.system('clear')
 
-    standard_params(1, 2)
-    standard_params(b=1, a=2)
+    # standard_params(1, 2)
+    # standard_params(b=1, a=2)
 
-    positional_only(1, 2)
-    # positional_only(b=1, a=2)   # ERROR: positional-only - true -ve
+    # positional_only(1, 2)
+    # # positional_only(b=1, a=2)   # ERROR: positional-only - true -ve
 
-    # keyword_only(1, 4)          # ERROR: keyword-only - true -ve
-    keyword_only(a=1, b=2)
+    # # keyword_only(1, 4)          # ERROR: keyword-only - true -ve
+    # keyword_only(a=1, b=2)
 
-    args = [3, 4]
-    combined_example(1, 2, kwd_only=3)
+    # args = [3, 4]
+    # combined_example(1, 2, kwd_only=3)
 
-    x = [1, 2, 3]
-    args_params(*x)
+    # a = [1, 2, 3]
+    # args_params(*a)
 
-    x = {'a': 1, 'b': 2, 'c': 3}
-    kwargs_params(**x)
+    b = {'a': 1, 'b': 2, 'c': 3}
+    # kwargs_params(**b)
+
+    combined_example(1, 2, kw_only=3, **b)
